2b5de756d6329a1c9b5f7e3b70e79b456f1f6bfb
Archly Volcy <avolcy@car8s1.42barcelona.com>
2024-07-02 13:52:24 +0200

    Just put 42 HEADERs

--- mod01/ex00/randomChump.cpp
+++ mod01/ex00/randomChump.cpp
@@ -1,8 +1,18 @@
-// 42HEADER
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 13:46:19 by avolcy            #+#    #+#             */
+/*   Updated: 2024/07/02 13:46:24 by avolcy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "Zombie.hpp"
+#include "Zombie.hpp"
 
-void randomChump( std::string name ) {
-
-  Zombie  random(name);
+void	randomChump(std::string name)
+{
+	Zombie random(name);
 }
