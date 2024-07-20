1fdbfdd582f23ad6b371b082bf4c0bcdfdc2c50e
ZenitsuTHB <starxyz12@gmail.com>
2024-07-21 00:20:29 +0200

    All done for BSP, Interactive mode successfully done

--- mod02/ex03/main.cpp
+++ mod02/ex03/main.cpp
@@ -18,6 +18,8 @@ int main( ) {
   int mode;
 
 
+  Fixed Xa, Ya, Xb, Yb, Xc, Yc, Xp, Yp;
+
   while (1) {
 
     std::cout << std::endl;
@@ -29,14 +31,8 @@ int main( ) {
       return(std::cerr << "Sorry ! command doesn't exist" << std::endl, 127);
     else if (mode == 2)
     {
-	    std::cout << std::endl;
-	    std::cout << "__________________BSP Interactive tests___________________"<< std::endl;
-	    std::cout << std::endl;
-
       int nbr;
       int arr[8];
-      Fixed arrObject[8];
-      Point arrOfObjectPoint[4];
       std::string coord[] = {"xA", "yA", "xB", "yB", "xC", "yC","xP","yP"};
 
       for (int i = 0; i < 8 ; i++) {
@@ -45,65 +41,50 @@ int main( ) {
         std::cout << coord[i] << " : ";
         std::cin>>nbr;
         arr[i] = nbr;
-        std::cout << "array[" << i << "] -> "<< arr[i] << std::endl;
       }
-      Fixed arrObject[0](arr[0]);
-      // arrObject[1](arr[1]);
-      // arrObject[2](arr[2]);
-      // arrObject[3](arr[3]);
-      // arrObject[4](arr[4]);
-      // arrObject[5](arr[5]);
-      // arrObject[6](arr[6]);
-      // arrObject[7](arr[7]);
-      Point A( arrObject[0], arrObject[1] );
-      Point B( arrObject[2], arrObject[3] );
-      Point C( arrObject[4], arrObject[5] );
-      Point P( arrObject[6], arrObject[7] );
-
-      std::cout << "The point A coordonates Overloaded are : ( " << A << " )" << std::endl;
-      std::cout << "The point B coordonates Overloaded are : ( " << B << " )" << std::endl;
-      std::cout << "The point C coordonates Overloaded are : ( " << C << " )" << std::endl;
-      std::cout << "The point P coordonates Overloaded are : ( " << P << " )" << std::endl;
-	    std::cout << std::endl;
-
-      if (bsp(A, B, C, P))
-        std::cout << "The Point P(Xp, Yp), lies inside of the triangle formed by ABC" << std::endl;
-      else
-        std::cout << "The Point P(Xp, Yp), isn't lying inside of the triangle formed by ABC" << std::endl;
-      break ;
+      Xa = Fixed(arr[0]);
+      Ya = Fixed(arr[1]);
+      Xb = Fixed(arr[2]);
+      Yb = Fixed(arr[3]);
+      Xc = Fixed(arr[4]);
+      Yc = Fixed(arr[5]);
+      Xp = Fixed(arr[6]);
+      Yp = Fixed(arr[7]);     
     }
-    else
-    {
-      std::cout << std::endl;
+    else {
+      Xa = Fixed(0);
+      Ya = Fixed(0);
+      Xb = Fixed(20);
+      Yb = Fixed(0);
+      Xc = Fixed(10);
+      Yc = Fixed(30);
+      Xp = Fixed(10);
+      Yp = Fixed(15);
+    }
+    Point A( Xa, Ya );
+    Point B( Xb, Yb );
+    Point C( Xc, Yc );
+    Point P( Xp, Yp );
+
+    system("clear");
+	  std::cout << std::endl;
+    if (mode == 1)
 	    std::cout << "__________________BSP Default tests___________________"<< std::endl;
-	    std::cout << std::endl;
-
-      Fixed Xa(0);
-      Fixed Ya(0);
-      Fixed Xb(20);
-      Fixed Yb(0);
-      Fixed Xc(10);
-      Fixed Yc(30);
-      Fixed Xp(10);
-      Fixed Yp(15);
-     
-      Point A( Xa, Ya );
-      Point B( Xb, Yb );
-      Point C( Xc, Yc );
-      Point P( Xp, Yp );
-      //Overload the insertion operator for Point if you rememberPoint
-      //Printing the coordonates using an Overloaded insertion
-      std::cout << "The point A coordonates Overloaded are : ( " << A << " )" << std::endl;
-      std::cout << "The point B coordonates Overloaded are : ( " << B << " )" << std::endl;
-      std::cout << "The point C coordonates Overloaded are : ( " << C << " )" << std::endl;
-      std::cout << "The point P coordonates Overloaded are : ( " << P << " )" << std::endl;
-	    std::cout << std::endl;
-      if (bsp(A, B, C, P))
-        std::cout << "The Point P(Xp, Yp), lies inside of the triangle formed by ABC" << std::endl;
-      else
-        std::cout << "The Point P(Xp, Yp), isn't lying inside of the triangle formed by ABC" << std::endl;
-      break ;
-    }
+    else
+	    std::cout << "__________________BSP Interactive tests___________________"<< std::endl;
+	  std::cout << std::endl;
+    //Overload the insertion operator for Point if you rememberPoint
+    //Printing the coordonates using an Overloaded insertion
+    std::cout << "The point A coordonates Overloaded are : ( " << A << " )" << std::endl;
+    std::cout << "The point B coordonates Overloaded are : ( " << B << " )" << std::endl;
+    std::cout << "The point C coordonates Overloaded are : ( " << C << " )" << std::endl;
+    std::cout << "The point P coordonates Overloaded are : ( " << P << " )" << std::endl;
+	  std::cout << std::endl;
+    if (bsp(A, B, C, P))
+      std::cout << "The Point P(Xp, Yp), lies inside of the triangle formed by ABC" << std::endl;
+    else
+      std::cout << "The Point P(Xp, Yp), isn't lying inside of the triangle formed by ABC" << std::endl;
+    break ;
   }
   std::cout << std::endl;
   return 0;
