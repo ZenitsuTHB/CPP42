53308553f5739266b2d0d14dfcc4cb1715b62e11
ZenitsuTHB <starxyz12@gmail.com>
2024-12-16 01:14:12 +0100

    flowchart and overloading and methods implemented

--- mod05/ex00/Buraucrat.hpp
+++ mod05/ex00/Buraucrat.hpp
@@ -15,19 +15,22 @@
 #define _BUREAUCRAT_HPP
 
 #include <string>
+#include <stdexcept>
 #include <iostream>
+
+# define lowestGrade 150
+# define highestGrade 1
 
 class Bureaucrat
 {
     private:
         int         _grade;
-        std::string _name;e
+        std::string _name;
 
     public:
         Bureaucrat(const std::string name, int grade);
         ~Bureaucrat();
 
-        int     setGrade(int grade);
         void    incrementGrade();
         void    decrementGrade();
         std::string getName();
@@ -35,27 +38,77 @@ class Bureaucrat
         
 };
 
-std::cout << getName() <<", bureaucrat grade " << getGrade() << std::endl;
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& cog)
+{
+  out << cog.getName() <<", bureaucrat grade" << cog.getGrade();
+  return (out);
+} 
+
+
 Bureaucrat::Bureaucrat(const std::string name, int grade)
 {
     _name = name;
-    if (grade > 150)
-        throw ();
+    if (grade > lowestGrade)
+      throw GradeTooHighException();
+    else if (grade < highestGrade)
+      throw GradeTooHighException();
+    _grade = grade;
 }
 
+void  Bureaucrat::incrementGrade()
+{
+  if (getGrade() > highestGrade)
+    _grade = _grade - 1;
+  throw GradeTooHighException();
+}
+
+void  Bureaucrat::decrementGrade()
+{
+  if (getGrade() < lowestGrade)
+    _grade = _grade + 1;
+  throw GradeTooLowException();
+}
+
+std::string Bureaucrat::getName()
+{
+  return(_name);
+}
+
+int Bureaucrat::getGrade()
+{
+  return (_grade);
+}
 
 Bureaucrat::GradeTooHighException()
 {
-  throw("Exception : Grade  is Too High");
+  std::cerr << "Exception : Grade  is Too High" << endl;
 }
 
 Bureaucrat::GradeTooLowException()
 {
-  throw("Exception : Grade is Too Low");
+  std::cerr << "Exception : Grade is Too Low" << endl;
 }
  
 Bureaucrat::~Bureaucrat()
 {
+}
+
+int main () {
+  
+  try {
+    Bureaucrat test("Testy", 7);
+    Bureaucrat testTooHigh("Testy", 1);
+    test.incrementGrade();
+    Bureaucrat testTooLow("Testy", 150);
+    test.decrementGrade();
+  }
+  catch (const std::exception&) {
+
+    std::cout << e.what() << endl;
+  }
+  std::cout << test << endl;
+
+  return 0;
 }
 
 #endif
