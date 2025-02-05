4887d149762442d42c07e66fe5077ea88c7cba24
ZenitsuTHB <starxyz12@gmail.com>
2025-02-05 14:39:35 +0100

    to have changes at 42 Cpp07 ex01 started

--- mod07/ex00/header/whatever.hpp
+++ mod07/ex00/header/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:38:19 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/05 08:54:29 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/05 09:33:14 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,19 @@ void	swap( T & x, T & y )
 template < typename T >
 T	& min( T & x, T & y )
 {
-	return(( x < y ) ? x : y);
+	if (x < y)
+		return (x);
+	return (y);
+	//return(( x < y ) ? x : y);
 }
 
 template < typename T >
 T	& max( T & x, T & y )
 {
-	return(( x > y ) ? x : y);
+	if (x > y)
+		return (x);
+	return (y);
+	//return(( x > y ) ? x : y);
 }	
 
 #endif
---
+++ mod07/ex01/header/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 13:42:36 by avolcy            #+#    #+#             */
+/*   Updated: 2025/02/05 13:42:36 by avolcy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _ITER_HPP_
+#define _ITER_HPP_
+
+# include <iostream>
+
+template <typename ARR, typename FUN>
+void iter(ARR &array, unsigned int length, FUN func)
+{
+    for (unsigned int i = 0; i < length; i++)
+        func(array[i]);
+}
+
+#endif
