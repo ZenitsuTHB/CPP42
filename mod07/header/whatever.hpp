dd371f316c42b943d71f32321d1116e3635c235c
ZenitsuTHB <starxyz12@gmail.com>
2025-02-05 09:00:19 +0100

    The first template ex0 finished the ex00 of cpp07

--- mod07/header/whatever.hpp
+++ mod07/header/whatever.hpp
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:38:19 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/04 21:17:07 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/05 08:54:29 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _WHATEVER_HPP_
 #define _WHATEVER_HPP_
 
+#include <iostream>
+#include <iomanip>
+
 template < typename T >
-T	swap( T & x, T & y )
+void	swap( T & x, T & y )
 {
 	T tmp;
 
@@ -26,13 +29,13 @@ T	swap( T & x, T & y )
 template < typename T >
 T	& min( T & x, T & y )
 {
-	return( x < y ) ? x : y;
+	return(( x < y ) ? x : y);
 }
 
 template < typename T >
 T	& max( T & x, T & y )
 {
-	return( x > y ) ? x : y;
-}
+	return(( x > y ) ? x : y);
+}	
 
 #endif
--- mod07/src/main.cpp
+++ mod07/src/main.cpp
@@ -3,36 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 21:17:21 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/04 22:50:55 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/05 08:57:59 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/whatever.h"
+#include "../header/whatever.hpp"
 
-int maini()
-{
-	int x, y;
-	float i, j;
-	double k, l;
+#include <iostream>
 
-	x = 42;
-	y = 24;
+int main() {
 
-	k = 42;
-	l = 24;
-
-	std::cout << swap( x,y ) << std::endl;	
-	std::cout << "max of :" <<  max( x, y) << std::endl;	
-	std::cout << "min of :" << min( x, y) << std::endl;	
-
-	std::cout << swap( x,y ) << std::endl;	
-	std::cout << "max of :" <<  max( x, y) << std::endl;	
-	std::cout << "min of :" << min( x, y) << std::endl;	
+	char a = 'a', b = 'b';
+	std::cout << "\nTHIS IS THE TEMPLATE TEST FOR CHAR\n";
+	std::cout << "Before swap 	a-----" << a << "	" << "b-----"<< b << std::endl;
+	swap(a, b);
+	std::cout << "After swap 	a-----" << a << "	" << "b-----"<< b << std::endl;
+	std::cout << "max of :	a-----" << a << "	b-----" << b << " is --> " << max(a, b) << std::endl;
+	std::cout << "min of :	a-----" << a << "	b-----" << b << " is --> " << min(a, b) << std::endl;
 	
-	std::cout << swap( x,y ) << std::endl;	
-	std::cout << "max of :" <<  max( x, y) << std::endl;	
-	std::cout << "min of :" << min( x, y) << std::endl;	
+	int x = 42, y = 24;
+	std::cout << "THIS IS THE TEMPLATE TEST FOR INT\n";
+    std::cout << "Before swap 	x-----" << x << "	" << "y-----"<< y << std::endl;
+    swap(x, y);
+    std::cout << "After swap 	x-----" << x << "	" << "y-----"<< y << std::endl;
+    std::cout << "max of :	x-----" << x << "	y-----" << y << " is --> " << max(x, y) << std::endl;
+    std::cout << "min of :	x-----" << x << "	y-----" << y << " is --> " << min(x, y) << std::endl;
+
+	float i = 42.9f, j = 24.9f;
+	std::cout << "\nTHIS IS THE TEMPLATE TEST FOR FLOAT\n";
+    std::cout << "Before swap 	i-----" << i << "	" << "j-----"<< j << std::endl;
+    swap(i, j);
+    std::cout << "After swap 	i-----" << i << "	" << "j-----"<< j << std::endl;
+	std::cout << "max of :	i-----" << i << "	j-----" << j << " is --> " << max(i, j) << std::endl;
+	std::cout << "min of :	i-----" << i << "	j-----" << j << " is --> " << min(i, j) << std::endl;
+  
+    double k = 4.29, l = 2.49;
+	std::cout << "\nTHIS IS THE TEMPLATE TEST FOR DOUBLE\n";
+	std::cout << "Before swap 	k-----" << k << "	" << "l-----"<< l << std::endl;
+	swap(k, l);
+	std::cout << "After swap 	k-----" << k << "	" << "l-----"<< l << std::endl;
+	std::cout << "max of :	k-----" << k << "	l-----" << l << " is --> " << max(k, l) << std::endl;
+	std::cout << "min of :	k-----" << k << "	l-----" << l << " is --> " << min(k, l) << std::endl;
+	
+    return 0;
 }
