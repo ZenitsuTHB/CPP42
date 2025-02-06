91645102afe643003aff387081441a8e22b8f3ee
ZenitsuTHB <starxyz12@gmail.com>
2025-02-06 17:53:09 +0100

    Array template started to finish at 42

--- mod07/ex02/Array.hpp
+++ mod07/ex02/Array.hpp
@@ -3,17 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:20:07 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/05 19:20:51 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/06 16:43:52 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _ARRAR_HPP_
 # define _ARRAR_HPP_
 
+#include <iostream>
+#include <exception>
 
+template < typename T>
+class Array {
+
+    private:
+
+    public:
+        Array() {
+            
+            _array = new T[0];
+        }
+}
+
+
+/*template <typename T>
+class Array
+{
+    private:
+        T *_arr;
+        unsigned int _size;
+    public:
+        Array() : _arr(NULL), _size(0) {}
+        Array(unsigned int n) : _arr(new T[n]), _size(n) {}
+        Array(const Array &src) : _arr(NULL), _size(0) { *this = src; }
+        ~Array() { delete [] _arr; }
+
+        Array &operator=(const Array &src)
+        {
+            if (this != &src)
+            {
+                delete [] _arr;
+                _size = src._size;
+                _arr = new T[_size];
+                for (unsigned int i = 0; i < _size; i++)
+                    _arr[i] = src._arr[i];
+            }
+            return *this;
+        }
+
+        T &operator[](unsigned int i)
+        {
+            if (i >= _size)
+                throw std::exception();
+            return _arr[i];
+        }
+
+        unsigned int size() const { return _size; }
+}
+*/
 #endif
 
 
--- mod07/ex02/main.cpp
+++ mod07/ex02/main.cpp
@@ -3,15 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:18:34 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/05 19:19:41 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/06 01:02:17 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/Array.hpp"
+#include "../header/Array.tpp"
+#include <iostream>
+
+
 
 int main()
 {
+    Array<int> arr(5);
+    Array<int> arr2(5);
+
+    for (unsigned int i = 0; i < arr.size(); i++)
+        arr[i] = i;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        arr2[i] = i + 5;
+
+    for (unsigned int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+
+    Array<int> arr3(arr2);
+    for (unsigned int i = 0; i < arr3.size(); i++)
+        std::cout << arr3[i] << " ";
+    std::cout << std::endl;
+
+    arr3 = arr;
+    for (unsigned int i = 0; i < arr3.size(); i++)
+        std::cout << arr3[i] << " ";
+    std::cout << std::endl;
+
+    try
+    {
+        arr[5] = 42;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Out of bounds" << std::endl;
+    }
+
+    return 0;    
 }
