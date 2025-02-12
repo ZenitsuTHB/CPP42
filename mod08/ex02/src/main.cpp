13f7fb60527611425c0654cb959be36b9a8310c9
ZenitsuTHB <starxyz12@gmail.com>
2025-02-12 17:24:55 +0100

    All done for cpp08

---
+++ mod08/ex02/src/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 17:04:09 by avolcy            #+#    #+#             */
+/*   Updated: 2025/02/12 17:04:09 by avolcy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../header/MutantStack.hpp"
+
+int main()
+{
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
+std::cout << mstack.size() << std::endl;
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+//[...]
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+++it;
+--it;
+while (it != ite)
+{
+std::cout << *it << std::endl;
+++it;
+}
+std::stack<int> s(mstack);
+return 0;
+}
