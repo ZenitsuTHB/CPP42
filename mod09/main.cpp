954a96c4c1af9ac6d9745cc8a8a7b8b591b8ed55
ZenitsuTHB <starxyz12@gmail.com>
2025-02-17 20:35:21 +0100

    startin bitcoin

--- mod09/main.cpp
+++ mod09/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 13:32:27 by avolcy            #+#    #+#             */
-/*   Updated: 2025/02/17 13:59:40 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/02/17 20:33:58 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,15 @@ int main(int argc, char *arg[])
 	std::string line;
 	if(argc != 2)
 		return(inputErr());
+	//input split by / char 
 	std::ifstream file("input.txt");
 	while(std::getline( file ,line ))
 		std::cout << line << "\n";
-	
+	//check file extension
+	//check file permission
+	//CHECK EACH LINES
+		//split date and value 
+		//check if date is correct 	
 
 	return (0);
 }
