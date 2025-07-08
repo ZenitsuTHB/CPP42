2e258ba938f74d378ba412bfed0b820d00c8681a
ZenitsuTHB <starxyz12@gmail.com>
2025-07-08 21:16:53 +0200

    Finally done and to be evaluated

--- mod09/ex02/header/PmergeMe.hpp
+++ mod09/ex02/header/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 15:28:26 by avolcy            #+#    #+#             */
-/*   Updated: 2025/07/07 21:57:56 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/07/08 21:16:04 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,15 @@ class PmergeMe
         Vector _vec;
         Deque  _deq;
         double _vecTime;
-        // double _deqTime;
+        double _deqTime;
 
-        void _printContainer(Vector& container, const std::string& msg) const;
         void _mergeInsertDeque(Deque& arr);
         void _mergeInsertVector(Vector& arr);
-        void _generateJacobsthalSequence(Vector& jacob, unsigned int size);
+        inline std::deque<std::pair<int, int > >    _makePair(Deque & arr);
+        inline std::vector<std::pair<int, int > >   _makePair(Vector & arr);
 
+        void _printContainer(Vector& container, const std::string& msg) const;
 
-        
     public:
         PmergeMe();
         ~PmergeMe();
@@ -61,24 +61,54 @@ class PmergeMe
             std::cout << "OK\n";
         }
         
-        template<typename T>
-        inline std::vector<std::pair<int, int > > makePair(const T & arr)
-        {
-            std::vector<std::pair<int, int> > pairs;
-            size_t limit = arr.size() - (arr.size() % 2);
-            for (size_t i = 0; i < limit; i += 2)
-            {
-               int a = arr[i];
-               int b = arr[i + 1];
-
-                if (a > b)
-                    pairs.push_back(std::make_pair(a, b));
-                else
-                    pairs.push_back(std::make_pair(b, a));
-            }
-
-            return pairs;
-        } 
+	template<typename T>
+	void _generateJacobsthalSequence(T & jacob, unsigned int size)
+	{
+	    if (size == 0 || size == 1)
+	        return;
+	    
+	    jacob.clear();
+	    jacob.push_back(0);
+	    for (unsigned int j0 = 0, j1 = 1; j1 < size;)
+	    {
+	        jacob.push_back(j1);
+	        unsigned int next = j1 + 2 * j0;
+	        j0 = j1;
+	        j1 = next;
+	    }
+	    
+	    // Efficiently fill missing indices using a merge algo approach
+	    T complete(size);
+	    for (unsigned int i = 0; i < size; ++i) {
+	        complete[i] = i;
+	    }
+	
+	    T merged;
+	    //merged.reserve(size);
+	    unsigned int j_idx = 0, c_idx = 0;
+	    while (j_idx < jacob.size() && c_idx < complete.size())
+	    {
+	        if (jacob[j_idx] < complete[c_idx])
+	            merged.push_back(jacob[j_idx++]);
+	        else if (jacob[j_idx] > complete[c_idx])
+	            merged.push_back(complete[c_idx++]);
+	        else 
+	        {
+	            merged.push_back(jacob[j_idx++]);
+	            c_idx++;
+	        }
+	    }
+	    
+	    while (j_idx < jacob.size()) merged.push_back(jacob[j_idx++]);
+	    while (c_idx < complete.size()) merged.push_back(complete[c_idx++]);
+	    
+	    jacob.clear();
+	    for (unsigned int i = 0; i < merged.size() && merged[i] < size; ++i) {
+	        if (i == 0 || merged[i] != merged[i-1]) {
+	            jacob.push_back(merged[i]);
+	        }
+	    }
+	}
 };
 
 #endif
--- mod09/ex02/src/PmergeMe.cpp
+++ mod09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 23:31:35 by avolcy            #+#    #+#             */
-/*   Updated: 2025/07/08 19:38:42 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/07/08 21:14:27 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,60 +35,77 @@ void PmergeMe::loadInput(int argc, char *argv[])
     if (_vec.empty())
         throw std::invalid_argument("No valid numbers provided.");
     _printContainer(_vec, "Before : ");
-    std::cout << "Processing a range of " << _vec.size() << std::endl;
 }
+
+inline std::deque<std::pair<int, int > > PmergeMe::makePair(Deque & arr)
+{
+    std::deque<std::pair<int, int> > pairs;
+    size_t limit = arr.size() - (arr.size() % 2);
+    for (size_t i = 0; i < limit; i += 2)
+    {
+        int a = arr[i];
+        int b = arr[i + 1];
+        if (a > b)
+            pairs.push_back(std::make_pair(a, b));
+        else
+            pairs.push_back(std::make_pair(b, a));
+    }
+    return pairs;
+} 
 
 void PmergeMe::_mergeInsertDeque(Deque& arr)
 {
-    (void)arr;
-}
-void PmergeMe::_generateJacobsthalSequence(Vector& jacob, unsigned int size)
-{
-    
-    if (size == 0 || size == 1)
+     if (arr.size() <= 1)
         return;
-    
-    jacob.clear();
-    jacob.push_back(0);
-    for (unsigned int j0 = 0, j1 = 1; j1 < size;) {
-        jacob.push_back(j1);
-        unsigned int next = j1 + 2 * j0;
-        j0 = j1;
-        j1 = next;
-    }
-    
-    // Efficiently fill missing indices using a merge algo approach
-    Vector complete(size);
-    for (unsigned int i = 0; i < size; ++i) {
-        complete[i] = i;
-    }
-
-    Vector merged;
-    merged.reserve(size);
-    unsigned int j_idx = 0, c_idx = 0;
-    while (j_idx < jacob.size() && c_idx < complete.size())
+    else if (arr.size() == 2)
     {
-        if (jacob[j_idx] < complete[c_idx])
-            merged.push_back(jacob[j_idx++]);
-        else if (jacob[j_idx] > complete[c_idx])
-            merged.push_back(complete[c_idx++]);
-        else 
-        {
-            merged.push_back(jacob[j_idx++]);
-            c_idx++;
-        }
+        if (arr[0] > arr[1])
+            std::swap(arr[0], arr[1]);
+        return;
+    }
+    Deque  biggest, smallest, jacob;
+    std::deque<std::pair<int, int > > pairsDeq = makePair(arr);
+    
+    biggest.clear();
+    smallest.clear();
+    for (size_t i = 0; i < pairsDeq.size(); ++i) {
+        biggest.push_back(pairsDeq[i].first);
+        smallest.push_back(pairsDeq[i].second);
     }
     
-    while (j_idx < jacob.size()) merged.push_back(jacob[j_idx++]);
-    while (c_idx < complete.size()) merged.push_back(complete[c_idx++]);
+    if (arr.size() % 2 != 0)
+        smallest.push_back(arr.back());
     
-    jacob.clear();
-    for (unsigned int i = 0; i < merged.size() && merged[i] < size; ++i) {
-        if (i == 0 || merged[i] != merged[i-1]) {
-            jacob.push_back(merged[i]);
-        }
+    _mergeInsertDeque(biggest);
+    _generateJacobsthalSequence(jacob, smallest.size());
+    for (unsigned int i = 0; i < jacob.size(); ++i)
+    {
+        unsigned int idx = jacob[i];
+        if (idx >= smallest.size())
+            continue;
+
+        int value = smallest[idx];
+        Deque::iterator pos = std::lower_bound(biggest.begin(),biggest.end(), value);
+        biggest.insert(pos, value);
     }
+    arr = biggest;
 }
+
+inline std::vector<std::pair<int, int > > PmergeMe::makePair(Vector & arr)
+{
+    std::vector<std::pair<int, int> > pairs;
+    size_t limit = arr.size() - (arr.size() % 2);
+    for (size_t i = 0; i < limit; i += 2)
+    {
+        int a = arr[i];
+        int b = arr[i + 1];
+        if (a > b)
+            pairs.push_back(std::make_pair(a, b));
+        else
+            pairs.push_back(std::make_pair(b, a));
+    }
+    return pairs;
+} 
 
 void PmergeMe::_mergeInsertVector(Vector& arr)
 {
@@ -102,7 +119,6 @@ void PmergeMe::_mergeInsertVector(Vector& arr)
     }
     
     Vector  biggest, smallest, jacob;
-    
     std::vector<std::pair<int, int> > pairs = makePair(arr);
     
     biggest.clear();
@@ -115,11 +131,7 @@ void PmergeMe::_mergeInsertVector(Vector& arr)
     if (arr.size() % 2 != 0)
         smallest.push_back(arr.back());
     
-    _printContainer(biggest, "=== B1 ===\n");
-    _printContainer(smallest, "==== S ====\n");
-    // std::sort(biggest.begin(), biggest.end());
     _mergeInsertVector(biggest);
-    
     _generateJacobsthalSequence(jacob, smallest.size());
     for (unsigned int i = 0; i < jacob.size(); ++i)
     {
@@ -130,9 +142,7 @@ void PmergeMe::_mergeInsertVector(Vector& arr)
         int value = smallest[idx];
         Vector::iterator pos = std::lower_bound(biggest.begin(),biggest.end(), value);
         biggest.insert(pos, value);
-        
     }
-    _printContainer(biggest, "=== BF ===\n");
     arr = biggest;
 }
 
@@ -144,15 +154,19 @@ void PmergeMe::sortAndDisplay()
     
     _vecTime = 	static_cast<double>(end - start) / CLOCKS_PER_SEC * 10;
 
+    start = clock();
+    _mergeInsertDeque(_deq);
+    end = clock();
+    
+    _deqTime = 	static_cast<double>(end - start) / CLOCKS_PER_SEC * 10;
+
     _printContainer(_vec, "After : ");
     std::cout << "Time to process a range of " << _vec.size()
-              << " elements with std::vector: " << _vecTime << " us" << std::endl;
-    // std::cout << "Time to process a range of " << _deq.size()
-    //           << " elements with std::deque: " << _deqTime << " us" << std::endl;
+              << " elements with std::vector : " << _vecTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << _deq.size()
+              << " elements with std::deque  : " << _deqTime << " us" << std::endl;
 }
 
-
-		
 // Step 1: Split into pairs
 // Step 2: Sort pairs by .first and rebuild biggest and smallest vectors
 // Step 3: Sort bigs using std::sort() or recursion
